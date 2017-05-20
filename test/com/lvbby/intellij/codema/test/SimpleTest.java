362df584b334bdeb97e16cd1f26b84db1c7ed26f
dushang.lp <dianxiangan@aliyun.com>
2017-05-20 10:39:54 +0800

    run

---
+++ test/com/lvbby/intellij/codema/test/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.lvbby.intellij.codema.test;
+
+import org.junit.Test;
+
+/**
+ * Created by dushang.lp on 2017/5/19.
+ */
+public class SimpleTest {
+
+    @Test
+    public void setst() {
+        System.out.println("sdf");
+    }
+}
